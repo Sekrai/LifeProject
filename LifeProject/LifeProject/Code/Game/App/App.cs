75efb0b2b73b6324cedebb71c0f59c23191fa5e5
André Dilles <andre.dilles@hotmail.com>
2018-05-26 21:27:02 +0200

    First Commit
    Bunch of empty classes

---
+++ LifeProject/LifeProject/Code/Game/App/App.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeProject
+{
+    class App
+    {
+        public App()
+        {
+
+        }
+
+        public void Init()
+        {
+
+        }
+
+        public void Update(GameTime someTime)
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
---
+++ LifeProject/LifeProject/Code/Utility/Renderer/Renderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeProject
+{
+    static class Renderer
+    {
+        public static void Init()
+        {
+
+        }
+
+        public static void Draw()
+        {
+
+        }
+    }
+}
